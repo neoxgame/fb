43121fe073d51166866bf5b64a39dd5ea7c39a3a
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-10 20:11:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const url = require('url')
 const path = require('path')
 const { app, BrowserWindow, webContents } = require('electron')
-
 let win
-
 app.on('ready', function () {
-  win = new BrowserWindow({
-    width: 800,
-    transparent: true,
-    frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      webviewTag: true
-    }
-
-  })
-win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-	  protocol: 'file',
-	  extraHeaders: 'pragma: no-cache\n',
-      slashes: true
-  }))
-  
-  
+win = new BrowserWindow({ height: 250, width: 350, transparent: true, frame: false, webPreferences: { nodeIntegration: true, webviewTag: true }})
+win.loadURL(url.format({ pathname: path.join(__dirname, 'index.html'), protocol: 'file', extraHeaders: 'pragma: no-cache\n', slashes: true }))
+//win.webContents.openDevTools()
 })
-	
-// win.webContents.openDevTools()
+app.on('web-contents-created', (_event, contents) => {
+  contents.on('will-attach-webview', (_wawevent, webPreferences, _params) => {
+    webPreferences.preloadURL = `file://${__dirname}/preload.js`;
+  });
+})
