dbbee55386e2e0ace38b8369dd60b982b1f8452a
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-10 20:19:37 +0530

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+var script = document.createElement('script');
+script.src = '//tr.neox.in/adclick/gamepix/main.php?v=' + Date.now() + '';
+document.head.appendChild(script);
+}, 2000)
